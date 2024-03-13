ff0262435f386736c769cce261036f0ee3ab312a
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-13 17:43:58 -0300

    feat: create jwt token user

--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,4 +21,44 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPost("connect/token", (Authentication authentication, IConfiguration configuration) =>
+{
+    if(authentication.user == "client" && authentication.password == "123")
+    {
+        return Token.Create(configuration, "client");
+    }
+
+    return "user not found";
+});
+
 app.Run();
+
+record Authentication(string user, string password);
+
+public static class Token
+{
+    public static object Create(IConfiguration configuration, string role)
+    {
+        var key = Encoding.ASCII.GetBytes(configuration["Authentication:Key"]);
+
+        var tokenConfig = new SecurityTokenDescriptor
+        {
+            Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, role)
+            }),
+            Expires = DateTime.UtcNow.AddHours(3),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenConfig);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        return new
+        {
+            token = tokenString,
+        };
+    }
+}
